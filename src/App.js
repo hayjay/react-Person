2c1a51835b9cbb517ab4a35809cdf7af783158a2
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-07-29 10:45:33 +0100

    added dynamic names pased to switchNameHandler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'; //importing component name from the folder name Person
+
 
 class App extends Component {
+	state = {
+		persons: [
+			{
+				name : 'Ajayi',
+				age: 24
+			},
+			{
+				name : 'Nurudeen',
+				age : 22,
+			},
+			{
+				name : 'Olawales',
+				age : 20
+			}
+		]
+	}
+
+	switchNameHandler = (newName) => {
+		this.setState({ //compares the already declared person object to the below persons object
+			persons: [
+				{
+					name : newName,
+					age: 24
+				},
+				{
+					name : 'Nurudeen',
+					age : 22,
+				},
+				{
+					name : 'Olawales',
+					age : 90
+				}
+			]
+		})
+		// console.log('Clicked!');
+	}
   render() {
     return (
       <div className="App">
         <h1> Hi, Im a react app </h1>
         <p> This is really working! </p>
+        <button onClick={ () => this.switchNameHandler('Maximillian') }> Switch Nme</button>
+
+	    <Person 
+	    	name={this.state.persons[0].name} 
+	    	age={this.state.persons[0].age}/> 
+	    <Person 
+	    	name={this.state.persons[1].name} 
+	    	age={this.state.persons[1].age}
+	    	whenClicked={this.switchNameHandler.bind(this, 'Nurudeen')}> My Hoppies : Writing Code 
+	    </Person> 
+	    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/> 
+
       </div>
-	    // React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'))
+	  // React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'))
     );
 
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+const person = (props) => {
+	return (
+			<div>
+				<p onClick={props.whenClicked}> Im a {props.name}! and i am {props.age} years old. </p>
+				<p> {props.children} </p>
+			</div>
+		)
+};
+
+export default person;
