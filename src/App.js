526cbd37990abc6b1b324a3604d48c41336b23f1
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-08-11 07:52:16 +0100

    implemented person update in a standard way without mutating the person object directly by using the spread opertor to achieve this

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
 	state = {
 		persons: [
 			{
+				id: 'ahhsjs23',
 				name : 'Ajayi',
 				age: 24
 			},
 			{
+				id: '9jsdksdsdsd',
 				name : 'Nurudeen',
 				age : 22,
 			},
 			{
+				id: 'jksdwwd922',
 				name : 'Olawales',
 				age : 20
 			}
@@ -23,29 +26,37 @@ class App extends Component {
 		showPersons : false
 	}
 
-	nameChangedHandler = (event) => {
-		this.setState({
-			persons : [
-				{
-					name : 'Ajayi',
-					age: 24
-				},
-				{
-					name : event.target.value,
-					age : 22,
-				},
-				{
-					name : 'Olawales',
-					age : 20
-				}
-			],
-			otherState : 'some other value'
-			
-			//set the person state to false by default not to show the person div
+	nameChangedHandler = (event, person_id) => {
+		//assigning the default persons array to a variable (persons) and looping through to find if the array of the persons matches the id passed in from the textbox
+		const personIndex = this.state.persons.findIndex((person) => { //find index helps find the index of an array and execute the find on each element of the array
+			return person.id === person_id; //returns true if the currently iterated findIndex person.id = the id passed in from the function
+		});
 
-		})
+		//process of updating state again
+		// 1. create a copy of the main array first
+		// 2. mutate maybe by splicing
+		// 3. then setTheState with setState
+
+		const person = {
+			//copying this without mutating the state
+			...this.state.persons[personIndex] //using the spread operator to copy objects to person variable just like instantiating the new obkect
+		};
+		//or using 
+		// const person = Object.assign({}, this.state.persons[personIndex]);
+		person.name = event.target.value;
+		const persons = [...this.state.persons]; //copy of the old array
+		persons[personIndex] = person;//defining the index on the go with a person value assigned
+
+		//update the person state
+		this.setState({
+			persons : persons
+		});
 	}
 
+	//process of updating state
+		// 1. create a copy of the main array first
+		// 2. mutate maybe by splicing
+		// 3. then setTheState with setState
 	deletePersonHandler = (personIndex) => {
 		// const persons = this.state.persons.slice();  // pull the array of persons then copy it and save into persons 
 		const persons = [...this.state.persons]; //using the spread operator helps copy the  declared persons array and paste into the persons variable
@@ -92,6 +103,8 @@ class App extends Component {
 									whenClicked = {() => this.deletePersonHandler(current_index)}
 									name={eachPerson.name}
 									age={eachPerson.age}
+									key={eachPerson.id} //to make sure and track the current index of each element we can manipulate with this
+									whenChanged={(event) => this.nameChangedHandler(event, eachPerson.id)}
 								/>
 					})
 				}
